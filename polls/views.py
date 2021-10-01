e004c7e7c898db66b10bca3cff55575fbe901eb9
Qianqiu Zhang <qianqiu@ualberta.ca>
2021-10-01 16:30:07 +0800

    done

--- polls/views.py
+++ polls/views.py
@@ -6,13 +6,13 @@ from django.views import generic
 from .models import Choice, Question
 
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+# class IndexView(generic.ListView):
+#     template_name = 'polls/index.html'
+#     context_object_name = 'latest_question_list'
+#
+#     def get_queryset(self):
+#         """Return the last five published questions."""
+#         return Question.objects.order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
